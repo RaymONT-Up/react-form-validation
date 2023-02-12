72c9cecec186c6d787c246059fa9068071d54af9
Raym0NT <konasr6@gmail.com>
2023-02-12 11:24:31 +0300

    input update

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,10 +1,43 @@
-const SomeInput = (props) => {
+import { useState } from "react";
+
+const SomeInput = props => {
+  const [enteredName, setEnteredName] = useState("");
+  const [wasNameInputTouched, setWasNameInputTouched] = useState(false);
+
+  const isEnteredNameValid = enteredName.trim() !== "";
+  const isNameInputInvalid = !isEnteredNameValid && wasNameInputTouched;
+
+  const nameInputChangeHandler = event => setEnteredName(event.target.value);
+  const nameInputLostFocusHandler = event => {
+    setWasNameInputTouched(true);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+
+    if (!isEnteredNameValid) return;
+
+    setEnteredName("");
+    setWasNameInputTouched(false);
+  };
+
+  const nameInputClasses = isNameInputInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className="form-control">
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor="name">Введите Имя</label>
-        <input type="text" id="name" />
+        <input
+          onBlur={nameInputLostFocusHandler}
+          onChange={nameInputChangeHandler}
+          type="text"
+          id="name"
+          value={enteredName}
+        />
       </div>
+      {isNameInputInvalid && <p className="error-text">Неверное имя</p>}
       <div className="form-actions">
         <button>Отправить</button>
       </div>
